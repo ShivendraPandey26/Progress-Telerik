f8fe3eca7b5eaff8168998fe2f4b4f79eb31c251
ShivendraPandey26 <aviralpandey7974@gmail.com>
2024-08-13 20:57:16 +0530

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
           UI frameworks and app development tools that 2.1 million developers
           love
         </h2>
-      <div className="scroll fixed bottom-5"></div>
+      <div className="scroll absolute bottom-5"></div>
       </div>
 
     </main>
